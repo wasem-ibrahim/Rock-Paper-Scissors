b337d3b30ae8e5ebb96158df5e48178426a8de01
wasem-ibrahim <wasem.ibrahim98@gmail.com>
2022-11-28 20:44:29 +0200

    beforeDesigningGame

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -33,6 +33,15 @@ export function Main() {
 
   return (
     <>
+      <nav className="flex justify-between items-center w-[98%] mx-auto py-4">
+        <img src="images/logo.png" alt="rps" className="w-20 h-20" />
+        <button
+          className="p-4 duration-300 border-2 rounded-lg cursor-pointer hover:bg-white hover:text-black border-white-400"
+          onClick={() => setShowAbout(true)}>
+          About
+        </button>
+      </nav>
+
       <main className="flex flex-col items-center justify-center h-screen text-6xl bg-gray-200">
         <div className="">
           <h1 className="py-4">Round {round}</h1>
